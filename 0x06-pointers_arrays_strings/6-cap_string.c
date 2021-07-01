795e1b6f454608dfcd5f756b41fd7462c6002b85
trustc0de <eche.echepeter@gmail.com>
2021-07-01 12:47:04 -0700

    6-cap string committed

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * cap_string - capitalize string
+ * @s: input string
+ * Return: Result string
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[0] >= 'a' && s[0] <= 'z')
+		{
+			s[0] = s[0] - 32;
+		}
+		if (s[i] == ' ' || s[i] == '.' ||
+				s[i] == '\n' || s[i] == '\t' ||
+				s[i] == ',' || s[i] == '!' ||
+				s[i] == ';' || s[i] == '?' ||
+				s[i] == '(' || s[i] == ')' ||
+				s[i] == '{' || s[i] == '}')
+		{
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
