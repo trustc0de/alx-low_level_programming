73102b944c6e115de70026715b3bc668934c36eb
trustc0de <eche.echepeter@gmail.com>
2021-07-05 11:43:32 -0700

    Strspn committed

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to be searched
+ * @accept: prefix to be measured
+ * Return: number of bytes in s which
+ * consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				bytes++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+				return (bytes);
+		}
+		s++;
+	}
+	return (bytes);
+}
