80798d16accbe94a719e5fe67cce700fbbbbc59a
trustc0de <eche.echepeter@gmail.com>
2021-07-19 07:52:53 -0700

    Main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  prints the  name of file
+ * Return: Always 0 (on success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
